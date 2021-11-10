d67fcba663d03776a0588b17a2f79e5e62c8c425
Nouran Ahmed <52378608+nouraan-ahmed@users.noreply.github.com>
2021-11-10 20:55:06 +0200

    Update Database

--- Data/Entities/Product.cs
+++ Data/Entities/Product.cs
@@ -12,6 +12,7 @@ namespace Online_MarketPlace_System.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public string Image { get; set; }
         public string Category { get; set; }
         public int Quantity { get; set; }
         public string Description { get; set; }
--- Data/Migrations/20211110181047_imageToProductTable.Designer.cs
+++ Data/Migrations/20211110181047_imageToProductTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Online_MarketPlace_System.Data;
 
 namespace Online_MarketPlace_System.Migrations
 {
     [DbContext(typeof(MarketplaceDbContext))]
-    partial class MarketplaceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211110181047_imageToProductTable")]
+    partial class imageToProductTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
